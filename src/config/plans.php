551321d17f480586a2143f593a23edecb35d4299
Robert Kummer <rok@ipunkt.biz>
2015-02-24 13:14:47 +0100

    more help text on configuration

--- src/config/plans.php
+++ src/config/plans.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * Plan configuration:
+ *
+ * [
+ *   PLAN-ID => [
+ *     'name' => Name of the plan,
+ *     'description' => Description of the plan,
+ *
+ *     'benefits' => [
+ *       FEATURE-NAME => [], // feature present, or:
+ *       FEATURE-NAME => [
+ *         'min' => 0,    // default, minimum margin value, optional
+ *         'max' => null, // default, maximum margin value, optional, unbounded by default
+ *       ],
+ *     ],
+ *   ],
+ * ],
+ *
+ */
 return [
 	'PLAN-ID' => [
 		'name' => 'TRIAL',
