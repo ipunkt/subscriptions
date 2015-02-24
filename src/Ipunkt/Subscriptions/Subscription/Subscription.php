69235443ef749abcf3e51d91f3d126aabe762f18
Robert Kummer <rok@ipunkt.biz>
2015-02-24 16:24:42 +0100

    Subscription entity / eloquent model; with migration

--- src/Ipunkt/Subscriptions/Subscription/Subscription.php
+++ src/Ipunkt/Subscriptions/Subscription/Subscription.php
@@ -7,9 +7,41 @@ use Illuminate\Database\Eloquent\Model;
  *
  * Subscription entity
  *
+ * @property integer $id
+ * @property integer $model_id
+ * @property string $model_class
+ * @property string $plan
+ * @property \Carbon\Carbon $trial_ends_at
+ * @property \Carbon\Carbon $subscription_ends_at
+ * @property string $state
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|Subscription whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|Subscription whereModelId($value)
+ * @method static \Illuminate\Database\Query\Builder|Subscription whereModelClass($value)
+ * @method static \Illuminate\Database\Query\Builder|Subscription wherePlan($value)
+ * @method static \Illuminate\Database\Query\Builder|Subscription whereTrialEndsAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Subscription whereSubscriptionEndsAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Subscription whereState($value)
+ *
  * @package Ipunkt\Subscriptions\Subscription
  */
 class Subscription extends Model
 {
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'model_id',
+		'model_class',
+	];
 
+	/**
+	 * The attributes that should be mutated to dates.
+	 *
+	 * @var array
+	 */
+	protected $dates = ['trial_ends_at', 'subscription_ends_at'];
 }
---
+++ src/migrations/2015_02_24_143018_add_subscriptions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSubscriptionsTable extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('subscriptions', function (Blueprint $table) {
+			$table->integer('id', true, true);
+			$table->unsignedInteger('model_id');
+			$table->string('model_class');
+			$table->string('plan');
+			$table->dateTime('trial_ends_at')->nullable();
+			$table->dateTime('subscription_ends_at');
+			$table->enum('state', ['active', 'canceled'])->default('active');
+			$table->timestamps();
+
+			$table->unique(['model_id', 'model_class', 'plan']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('subscriptions');
+	}
+}
