cf717526edd1575a248d11bf0d76fda356ff42a9
Robert Kummer <rok@ipunkt.biz>
2015-02-24 15:31:45 +0100

    added documentation to the provided view

--- src/views/benefits/list.blade.php
+++ src/views/benefits/list.blade.php
@@ -1,8 +1,20 @@
-@foreach ($benefits as $benefit)
+<?php
+/**
+ * Using example:
+
+<ul class="list-unstyled">
+@include('subscriptions::benefits.list', ['benefits' => $plan->benefits()])
+</ul>
+
+ * @view subscriptions::benefits.list
+ * @param Collection|Benefit[] $benefits
+ * @supports translation in subscriptions.features.[YOUR-FEATURE-ID]
+ */
+?>@foreach ($benefits as $benefit)
 	<li>@if (Lang::has('subscriptions.features.' . $benefit->feature()))
-		@lang('subscriptions.features.' . $benefit->feature())
+			@lang('subscriptions.features.' . $benefit->feature())
 		@else
-		{{ $benefit->feature() }}
+			{{ $benefit->feature() }}
 		@endif
 		<span class="pull-right">
 			@if ($benefit->max() === null)
